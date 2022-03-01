f89451275689e322daa7fa9a06e815b5a7984432
brunogomes98 <brunog.vix@gmail.com>
2022-03-01 01:31:23 -0300

    updated the names and the imports

--- backend_sanity/schemas/schema.js
+++ backend_sanity/schemas/schema.js
@@ -3,7 +3,13 @@ import createSchema from 'part:@sanity/base/schema-creator'
 
 // Then import schema types from any plugins that might expose them
 import schemaTypes from 'all:part:@sanity/base/schema-type'
+import works from './works'
 import testimonials from './testimonials'
+import brands from './brands'
+import skills from './skills'
+import workExperience from './workExperience'
+import contact from './contact'
+import experiences from './experiences'
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -12,6 +18,13 @@ export default createSchema({
   // Then proceed to concatenate our document type
   // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([
-    testimonials
+    testimonials,
+    works,
+    brands,
+    about,
+    skills,
+    workExperience,
+    contact,
+    experiences
   ]),
 })
